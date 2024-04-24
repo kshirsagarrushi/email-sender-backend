4c01163f713fa07667b1ce272ef6ee8c74f58687
Rushikesh Kshirsagar <108231382+kshirsagarrushi@users.noreply.github.com>
2024-04-24 21:39:31 +0530

    baseurl updated

--- frontend/src/MyForm.jsx
+++ frontend/src/MyForm.jsx
@@ -18,7 +18,7 @@ export default function MyForm() {
   const [subject, setSubject] = useState("");
   const [file, setFile] = useState(null);
   const [emails, setEmails] = useState([]);
-  const baseUrl = "http://localhost:8000";
+  const baseUrl = "https://email-sender-backend-zjae.vercel.app/";
 
   const handleUpload = (event) => {
     if (!file) {
