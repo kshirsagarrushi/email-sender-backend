1efd204eb35237c20c6182428b4a20d5a8a13699
Rushikesh Kshirsagar <108231382+kshirsagarrushi@users.noreply.github.com>
2024-04-26 19:44:46 +0530

    sendEmail url changed

--- frontend/src/MyForm.jsx
+++ frontend/src/MyForm.jsx
@@ -18,7 +18,7 @@ export default function MyForm() {
   const [subject, setSubject] = useState("");
   const [file, setFile] = useState(null);
   const [emails, setEmails] = useState([]);
-  const baseUrl = "https://vercel.com/kshirsagarrushis-projects/email-sender-server/5aX2S3GsKHq9MtgLPKutzojWjZVD";
+  const baseUrl = "https://email-sender-server-dusky.vercel.app";
 
   const handleUpload = (event) => {
     if (!file) {
